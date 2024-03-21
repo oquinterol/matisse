2542ae239f549ce0a42f063e9d3c8178d49978e2
Alexis Quintero <oquinterol@unal.edu.co>
2024-03-21 11:41:16 -0500

    try again fixing routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://quinterol.github.io',
+  base: '/matisse',
+  trailingSlash: 'always',
   experimental: {
     viewTransitions: true,
   },
   integrations: [tailwind(), compress()],
-  site: 'https://quinterol.github.io',
-  base: '/matisse',
 });
